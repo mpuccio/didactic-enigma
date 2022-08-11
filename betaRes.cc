88bda6608b44665ba0499a64d4e973c864a57310
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2022-08-11 19:36:45 +0200

    Make betaRes work with the new rootlogon

--- betaRes.cc
+++ betaRes.cc
@@ -18,21 +18,38 @@ void BinLogAxis(TH1 *h) {
   delete[] newBins;
 }
 
-void betaRes(float timeres=20.e-12f, float l = 0.4) {
+void betaRes(float timeres=35.e-12f, float l = 0.39) {
   TCanvas* cv = new TCanvas();
-  TH2D* res = new TH2D("res",";#it{p} (GeV/#it{c}); #beta;", 2000, 0.1, 20.1, 360, 0.2, 1.1); 
-  TH2D* res2 = new TH2D("res2",";#it{p} (GeV/#it{c}); #beta;", 2000, 0.1, 20.1, 360, 0.2, 1.1); 
+  TH2D* res = new TH2D("res",";#it{p} (GeV/#it{c}); #beta;", 2000, 0.1, 20.1, 360, 0.2, 1.1);
+  TH2D* res2 = new TH2D("res2",";#it{p} (GeV/#it{c}); #beta;", 2000, 0.1, 20.1, 360, 0.2, 1.1);
+
   BinLogAxis(res);
   BinLogAxis(res2);
+  TH2D* nsigma[9], *species[9];
+  for (int i{0}; i < __NumParticles; ++i) {
+    species[i] = new TH2D(Form("species%s", __ParticleName[i]), ";#it{p} (GeV/#it{c}); #beta;", 2000, 0.1, 20.1, 360, 0.2, 1.1);
+    nsigma[i] = new TH2D(Form("nsigma%s", __ParticleName[i]), ";#it{p} (GeV/#it{c}); n#sigma;", 2000, 0.1, 20.1, 400,-5,5);
+    BinLogAxis(nsigma[i]);
+    BinLogAxis(species[i]);
+  }
+
   for (int iP{1}; iP <= res->GetNbinsX(); ++iP) {
     TAxis* pAx = res->GetXaxis();
-    float mom = pAx->GetBinCenter(iP);  
+    float mom = pAx->GetBinCenter(iP);
     for (int i{0}; i <= 8; ++i) {
-      double origB{mom / std::hypot(AliPID::ParticleMass(i), mom)}; 
+      double origB{mom / std::hypot(__ParticleMass[i], mom)};
       double origT{l / (origB * TMath::C())};
-      TH2* hist = AliPID::ParticleCharge(i) == 1 ? res : res2;
+      TH2* hist = __ParticleCharge[i] == 1 ? res : res2;
       for (int j{0}; j < 1000; ++j) {
-        hist->Fill(mom, l / ((origT + gRandom->Gaus(0., timeres)) * TMath::C())); 
+        double modT = origT + gRandom->Gaus(0., timeres);
+        double beta = l / (modT * TMath::C());
+        hist->Fill(mom, beta);
+        species[i]->Fill(mom, beta);
+        for (int k{0}; k <= 8; ++k) {
+          double bTH{mom / std::hypot(__ParticleMass[k], mom)};
+          double tTH{l / (bTH * TMath::C())};
+          nsigma[i]->Fill(mom, (modT - tTH) / timeres);
+        }
       }
     }
   }
@@ -43,4 +60,5 @@ void betaRes(float timeres=20.e-12f, float l = 0.4) {
   cv2->SetLogx();
   res2->Draw("colz");
   cv2->SaveAs("cv2.png");
+  nsigma[4]->Draw("colz");
 }
