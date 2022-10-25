f2487dc8a4329c5733f498212f672561b5dd35fc
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2022-10-25 16:41:28 +0900

    Add fast check on kink resolution

--- genDecay.cc
+++ genDecay.cc
@@ -6,10 +6,12 @@
 #include <TMath.h>
 #include <TRandom.h>
 
-constexpr int kNtrials{10000};
+constexpr int kNtrials{1000000};
 constexpr double kHypMass{2.99125};
 constexpr double kTritonMass{2.80892};
 constexpr double kPi0Mass{0.139};
+constexpr double kPtResKink{0.2};
+constexpr double kPtRes{0.02};
 
 void genDecay() {
   gRandom->SetSeed(1234);
@@ -22,8 +24,9 @@ void genDecay() {
   TH1D hTritonPt("hTritonPt", ";#it{p}_{T} (GeV/#it{c});Entries", 100, 0, 10);
   TH1D hPi0Pt("hPi0Pt", ";#it{p}_{T} (GeV/#it{c});Entries", 100, 0, 2);
   TH1D hGammaPt("hGammaPt", ";#it{p}_{T} (GeV/#it{c});Entries", 100, 0, 2);
+  TH2D hHyperMkink("hHyperMkink", ";#it{p}_{T} (GeV/#it{c});M (GeV/#it{c}^{2});Entries", 10, 0, 5, 300, 2.9, 3.2);
 
-  TLorentzVector mother;
+  TLorentzVector mother, kinkPi0, recTriton;
   TGenPhaseSpace genH, genPi0;
   const double massesH[2]{kTritonMass, kPi0Mass}, massesPi0[2]{0.,0.};
   for (int i{0}; i < kNtrials; ++i) {
@@ -36,10 +39,17 @@ void genDecay() {
     genPi0.Generate();
     hGammaPt.Fill(genPi0.GetDecay(0)->Pt());
     hGammaPt.Fill(genPi0.GetDecay(1)->Pt());
+    double momPt{mother.Pt()};
+    mother.SetPtEtaPhiM(gRandom->Gaus(momPt, momPt * kPtResKink), mother.Eta(), mother.Phi(), kHypMass);
+    recTriton.SetPtEtaPhiM(gRandom->Gaus(genH.GetDecay(0)->Pt(), genH.GetDecay(0)->Pt() * kPtRes), genH.GetDecay(0)->Eta(), genH.GetDecay(0)->Phi(), kTritonMass);
+    TLorentzVector kinkPi0 = mother - recTriton;
+    kinkPi0.SetPtEtaPhiM(kinkPi0.Pt(), kinkPi0.Eta(), kinkPi0.Phi(), kPi0Mass); // make sure that we know the mass
+    hHyperMkink.Fill(momPt, (recTriton+kinkPi0).M());
   }
 
   TFile output("output.root", "recreate");
   hTritonPt.Write();
   hPi0Pt.Write();
   hGammaPt.Write();
+  hHyperMkink.Write();
 }
